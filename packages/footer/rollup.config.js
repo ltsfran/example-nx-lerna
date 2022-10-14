d553b60d5b11587afced49a50730c7e9a9b6d8c4
Luis Tupa <luis.tupa@neoauto.pe>
2022-10-14 12:32:20 -0500

    feat: add type config in rollup

--- packages/footer/rollup.config.js
+++ packages/footer/rollup.config.js
@@ -1,16 +1,27 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from 'rollup-plugin-typescript2';
+const packageJSON = require("./package.json");
 
 export default {
     input: ["src/index.tsx"],
     output: [
         {
-            dir: "dist",
-            entryFileNames: "[name].js",
+            file: packageJSON.main,
             format: "cjs",
-            exports: "named"
+            sourcemap: true
+        },
+        {
+            file: packageJSON.module,
+            format: "esm",
+            sourcemap: true
         }
     ],
     plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
         typescript(),
     ],
     external: ["react"]
